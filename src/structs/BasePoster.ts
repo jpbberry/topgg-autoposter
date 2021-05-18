ba18e8395d5a9645ca392b1d2126285c67fc524d
Piyush Bhangale <bhangalepiyush@gmail.com>
2021-05-18 19:54:05 +0530

    request changes

--- src/structs/BasePoster.ts
+++ src/structs/BasePoster.ts
@@ -88,6 +88,6 @@ export class BasePoster extends EventEmitter {
   public async post () {
     this.api.postStats(await this.binds.getStats())
       .then((data) => this.emit('posted', data))
-      .catch((err) => this.emit("error", err))
+      .catch((err) => "error" in this._events ? this.emit("error", err) : console.error(err))
   }
 }
